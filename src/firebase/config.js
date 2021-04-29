8715440bc1f5f6963ae5dfe7533542069c088eae
Andriantsimba <mami28niaina@gmail.com>
2021-04-29 13:03:23 +0700

    Week12 Practicum

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAErxx6cjDHgERSiMHV0XFyL8eN5wu8PtM",
+    authDomain: "pbfweek11.firebaseapp.com",
+    databaseURL: "https://pbfweek11-default-rtdb.firebaseio.com",
+    projectId: "pbfweek11",
+    storageBucket: "pbfweek11.appspot.com",
+    messagingSenderId: "179709680596",
+    appId: "1:179709680596:web:3c1ca3a0fd6e6f0ad59441"
+  };
+
+  export default firebaseConfig;
